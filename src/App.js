2e6ec9986879ec3b92a97dd4c9550dd364c8927d
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-05-11 20:18:04 +0600

    msg

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   <div>
+      <Home/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+import YearData from '.././../Data/Data.json'
+import { Button } from 'react-bootstrap';
+import LaunchCart from './LaunchCart/LaunchCart';
+
+
+const Home = () => {
+    const [launch, setLaunch] = useState([]);
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches?limit=100')
+            .then(res => res.json())
+            .then(data => setLaunch(data))
+    }, [])
+    return (
+        <div className='home'>
+            <h3>Space-X Programs</h3>
+            <div className="row">
+                <div className="col-lg-3">
+                    <h5>Filters</h5>
+                    <div>
+                        <h5 className='heading'>Launch Year</h5>
+                        <div className="row g-2 year">
+                            {
+                                YearData.map(year =>
+                                    <div className="col-6 text-center mb-2 pe-3">
+                                        <Button>{year.year}</Button>
+                                    </div>)
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <h5 className='heading'>Successful Launch </h5>
+                        <div className="row g-2 year">
+                            <div className="col-6 text-center mb-2 pe-3">
+                                <Button>True</Button>
+                            </div>
+                            <div className="col-6 text-center mb-2 pe-3">
+                                <Button className='bg-gray'>False</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-9">
+                    <div className="row">
+                        {
+                           launch.map(singleLaunch=> <LaunchCart singleLaunch={singleLaunch}/>)
+                        }
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
