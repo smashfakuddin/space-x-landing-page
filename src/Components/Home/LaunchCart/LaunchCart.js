8edee10547a35b84648298a07f1bf2d5e32611ff
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-05-12 16:46:09 +0600

    final commit

--- src/Components/Home/LaunchCart/LaunchCart.js
+++ src/Components/Home/LaunchCart/LaunchCart.js
@@ -7,8 +7,8 @@ const LaunchCart = ({ singleLaunch }) => {
     console.log(launch_success);
 
     return (
-        <div className='col-lg-2 col-md-4 cart'>
-            <img  src={mission_patch} alt="" width='' />
+        <div className='col-lg-2 col-md-4 col-sm-9 col-10 cart'>
+            <img className='img-fluid'  src={mission_patch} alt="" width='' />
             <h6 className='cart-heading'>{mission_name} # {flight_number}</h6>
             <h6> <strong> Misson Id:</strong> {singleLaunch.mission_id[0] }</h6>
             <h6><strong>Launch Year:</strong>   {launch_year}</h6>
